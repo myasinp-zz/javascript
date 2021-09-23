b94fc1d2b62c1a7f0cad78b51e17a035dd1028a9
myasinp <mohammedyasinpzero@example.com>
2021-09-23 15:23:40 +0530

    added test file

---
+++ test.js
@@ -0,0 +1,13 @@
+var arr = [2,1,2,2,3,3,3,5]
+var dict = {}
+
+for (i=0; i<arr.length; i++){
+    if (dict[arr[i]]){
+        dict[arr[i]]++
+    }
+    else {
+        dict[arr[i]] = 1
+    }
+}
+
+console.log(dict);
